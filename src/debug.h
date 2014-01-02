b589feba2bceb74eb2f33215f0d019dcd88a19c9
Filip <kobronson@gmail.com>
2014-01-02 16:54:42 +0100

    Dodanie dbg

---
+++ src/debug.h
@@ -0,0 +1,8 @@
+#ifndef DEBUG_H_
+#define DEBUG_H_
+
+#define DBG_LINE printf("%s:%d\n",__PRETTY_FUNCTION__,__LINE__);
+#define DBG(...) printf( __VA_ARGS__)
+
+
+#endif /* DEBUG_H_ */
--- src/graph.c
+++ src/graph.c
@@ -6,11 +6,12 @@
  */
 
 #include "graph.h"
+#include "debug.h"
 #include <stdlib.h>
 
 #define MAX_CHARS 64
 
-#define DEBUG printf("%s:%d\n",__PRETTY_FUNCTION__,__LINE__);
+
 
 
 uint16_t getVerticesCount(char *filename)
@@ -54,7 +55,7 @@ Graph* createGraph(char *filename)
 	g  = malloc( size * sizeof( struct t_Edge *));
 	fp = fopen(filename, "r");
 	
-	DEBUG
+
 	for(i=0;i<size;i++)
 	{
 		g[i] = NULL ;
