03d06f1cca580f0334395a0433e1bc289bc60be2
Filip <kobronson@gmail.com>
2014-01-07 23:02:25 +0100

    Kruskal szkielet

---
+++ src/kruskal.c
@@ -0,0 +1,12 @@
+#include "kruskal.h"
+#include "set.h"
+#include "debug.h"
+#include <stdio.h>
+
+
+Graph* kruskal(Graph *graph, uint16_t size){
+
+
+
+
+}
---
+++ src/kruskal.h
@@ -0,0 +1,12 @@
+#ifndef KRUSKAL_H_
+#define KRUSKAL_H_
+
+#include <stdint.h>
+#include "graph.h"
+
+
+
+Graph* kruskal(Graph *graph, uint16_t size);
+
+
+#endif /* KRUSKAL_H_ */
