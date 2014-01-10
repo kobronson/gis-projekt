e441841bf55eab237e3e9b5bd4b176f508065d3a
Filip <kobronson@gmail.com>
2014-01-10 16:54:42 +0100

    Zbiory rozlaczne

--- src/kruskal.h
+++ src/kruskal.h
@@ -5,6 +5,14 @@
 #include "graph.h"
 
 
+
+/*Disjoint data set - warunek nie moze byc cykli w grafie*/
+
+struct disj_set{
+	int parent;
+	int vertice;
+};
+
 int compareEdges(const void* edge_1, const void* edge_2);
 EGraph* kruskal(EGraph *graph);
 
